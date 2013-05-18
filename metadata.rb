714e513b6862a39efa92bfe301f179e0015a20e5
ysasaki <aloelight@gmail.com>
2013-05-18 12:18:18 +0900

    Adds recipe attribute in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'MIT License'
 description      'Installs/Configures movabletype-opensource'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
+
+recipe 'movabletype-opensource', "Installs/Configures movabletype-opensource";
