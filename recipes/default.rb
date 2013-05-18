0b3f3ad00fa4e54a172d0fe88c122f5e8c67bafe
ysasaki <aloelight@gmail.com>
2013-05-18 13:53:20 +0900

    Fixes do..end

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,28 +11,27 @@ domain = node['movabletype-opensource']['domain']
 owner  = node['movabletype-opensource']['owner']
 group  = node['movabletype-opensource']['group']
 
-directory "/var/www/#{domain}" do {
+directory "/var/www/#{domain}" do
     owner "#{owner}"
     group "#{group}"
     action :create
-}
+end
 
 %w{ htdocs logs cgi-bin }.each do |dir| 
-    directory "/var/www/#{domain}/#{dir}" do {
+    directory "/var/www/#{domain}/#{dir}" do 
         owner "#{owner}"
         group "#{group}"
         action :create
-    }
     end
 end
 
-git "/var/www/#{domain}/cgi-bin/mt" do {
+git "/var/www/#{domain}/cgi-bin/mt" do 
     repository "git://github.com/movabletype/movabletype.git"
     reference "master"
     action :checkout
     user "#{owner}"
     group "#{group}"
-}
+end
 
 template "/var/www/#{domain}/cgi-bin/mt-config.cgi" do 
     source "mt-config.cgi"
