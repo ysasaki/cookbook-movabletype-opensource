5e993fb1c5062d2756a120aac667868030a15787
ysasaki <aloelight@gmail.com>
2013-05-18 14:04:03 +0900

    Fixes License

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'movabletype-opensource'
 maintainer       'aloelight'
 maintainer_email 'aloelight at gmail.com'
-license          'MIT License'
+license          'MIT'
 description      'Installs/Configures movabletype-opensource'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
