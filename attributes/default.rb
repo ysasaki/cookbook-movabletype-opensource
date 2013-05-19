350684e4d3d86640f4bf2107b81f247479822f60
ysasaki <aloelight@gmail.com>
2013-05-20 01:37:16 +0900

    Fixes init-script

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,14 +7,26 @@
 # LICENSE: MIT License
 # 
 
+# VirtualHost
 default['movabletype-opensource']['domain'] = "mt.example.com"
+
+# for installation and web workers
 default['movabletype-opensource']['owner'] = "apache"
 default['movabletype-opensource']['group'] = "apache"
+
+# MT MySQL
 default['movabletype-opensource']['database'] = "movabletype"
 default['movabletype-opensource']['dbuser'] = "movabletype"
 default['movabletype-opensource']['dbpassword'] = "movabletype"
 default['movabletype-opensource']['dbpassword'] = "movabletype"
+
+# MT Default language
 default['movabletype-opensource']['default_language'] = "ja"
-default['movabletype-opensource']['psgi_port'] = 8080
+
+# Starman
+default['movabletype-opensource']['workers'] = 2
+default['movabletype-opensource']['psgi_port'] = 80
+
+# perl
 default['movabletype-opensource']['perl_install_path'] = "/usr/local/perl-5.16"
 default['movabletype-opensource']['perl_version'] = "5.16.3"
